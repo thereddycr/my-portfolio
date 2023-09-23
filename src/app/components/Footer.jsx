4a0a488fd92ad963d03cb5ffda2a267e8891ba2d
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-09-23 22:07:37 +0530

    my-portfolio

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between text-slate-600">
-        <span>Made with ❤️</span>
-        <p className="text-slate-600">Copyright © 2023 Chandu Reddy. All rights reserved.</p>
+      <div className="container p-5 flex justify-between text-slate-600 w-full">
+        <span className="text-xs w-40 sm:text-sm">Made with ❤️</span>
+        <p className="text-slate-600 text-sm">
+          Copyright © 2023 Chandu Reddy. All rights reserved.
+        </p>
       </div>
     </footer>
   );
