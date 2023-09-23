71e32fd85838b9e607a0bab42718e57602ef1172
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-09-23 21:20:51 +0530

    made changes in responsive and added portfolio....

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -3,6 +3,9 @@ import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 
 const projectsData = [
   {
@@ -112,14 +115,49 @@ const Projects = () => {
             animate={isInView ? "animate" : "initial"}
             transition={{ duration: 0.3, delay: index * 0.4 }}
           >
-            <ProjectCard
+            <div>
+              <div
+                className="h-52 md:h-72 rounded-t-xl relative group"
+                // style={{
+                //   background: `url(${imgUrl})`,
+                //   backgroundSize: "cover",
+                //   resize: "cover",
+                // }}
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  layout="fill"
+                  objectFit="cover"
+                />
+                <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+                  <Link
+                    href={project.gitUrl}
+                    className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+                  >
+                    <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+                  </Link>
+                  <Link
+                    href={project.previewUrl}
+                    className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+                  >
+                    <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+                  </Link>
+                </div>
+              </div>
+              <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+                <h5 className="text-xl font-semibold mb-2">{project.title}</h5>
+                <p className="text-[#ADB7BE]">{project.description}</p>
+              </div>
+            </div>
+            {/* <ProjectCard
               key={project.id}
               title={project.title}
               description={project.description}
               imgUrl={project.image}
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
-            />
+            /> */}
           </motion.li>
         ))}
       </ul>
