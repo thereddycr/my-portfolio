8ac173d756dd5a55841b759409cf3390f32a9bb6
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-09-23 21:33:43 +0530

    made changes in responsive and added portfolio.........

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -3,9 +3,6 @@ import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 
 const projectsData = [
   {
@@ -13,7 +10,7 @@ const projectsData = [
     title: "Shadow Properties Website",
     description:
       "Real-estate CRM for lead generation, and marketing automation. Shadow Properties is the future of real estate investment. Our easy-to-use app streamlines everything, and it is available at your fingertips, which gives you all the information you manually search to sell or rent houses. Our tool is apt for individuals and teams that believe in real estate as a business. It is simple to use and can result in deals that can change your life.",
-    image: "/images/shadow/house-1.avif",
+    image: "./images/shadow/house-1.avif",
     tag: ["All", "Web"],
     gitUrl: "https://web.shadow.properties/",
     previewUrl: "https://web.shadow.properties/",
@@ -23,7 +20,7 @@ const projectsData = [
     title: "Buyers Landing Page",
     description:
       "This landing page specifically for buyers within the Shadow Properties app, with a focus on enhancing buyer management.",
-    image: "/images/shadow/house-2.avif",
+    image: "./images/shadow/house-2.avif",
     tag: ["All", "Web"],
     gitUrl:
       "https://webtest.shadow.properties/contacts/6501503d4f8fbed7c8f0ca19",
@@ -35,7 +32,7 @@ const projectsData = [
     title: "My Portfolio",
     description:
       "Welcome to my portfolio showcasing my work as a frontend developer. Explore a collection of web and mobile projects that highlight my skills and passion for creating visually stunning and user-friendly interfaces. Whether you're looking for web development, mobile app design, or UI development, you'll find examples of my diverse expertise here.",
-    image: "/images/shadow/portfolio.png",
+    image: "./images/shadow/portfolio.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
@@ -45,7 +42,7 @@ const projectsData = [
     title: "Shadow Properties Android App",
     description:
       "Explore the future of real estate investment with our Android app. Shadow Properties is your comprehensive real-estate CRM for lead generation and marketing automation. Our easy-to-use app brings everything you need to your fingertips, providing you with all the information required to sell or rent houses effortlessly. Whether you're an individual or part of a real estate team, our tool simplifies the process and can lead to life-changing deals.",
-    image: "/images/shadow/house-3.avif",
+    image: "./images/shadow/house-3.avif",
     tag: ["All", "Mobile"],
     gitUrl:
       "https://play.google.com/store/apps/details?id=and.app.properties.shadow",
@@ -57,7 +54,7 @@ const projectsData = [
     title: "Shadow Properties IOS App",
     description:
       "Discover the future of real estate investment with our iOS app. Shadow Properties offers a comprehensive real-estate CRM designed for lead generation and marketing automation. Our user-friendly app puts everything at your fingertips, providing all the information you need for selling or renting houses effortlessly. Whether you're an individual or part of a real estate team, our tool simplifies the process and can lead to transformative deals.",
-    image: "/images/shadow/house-5.avif",
+    image: "./images/shadow/house-5.avif",
     tag: ["All", "Mobile"],
     gitUrl:
       "https://itunes.apple.com/us/app/shadow-properties-ios/idXXXXXXXXXX",
@@ -107,46 +104,24 @@ const Projects = () => {
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {/* {filteredProjects.map((project, index) => ( */}
-        <>
-          <div>
-            <div
-              className="h-52 md:h-72 rounded-t-xl relative group"
-              // style={{
-              //   background: `url(${imgUrl})`,
-              //   backgroundSize: "cover",
-              //   resize: "cover",
-              // }}
-            >
-              <Image
-                key={"1"}
-                src={"/images/shadow/house-1.avif"}
-                alt={"Shadow Properties Website"}
-                layout="fill"
-                objectFit="cover"
-              />
-              {/* <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-                  <Link
-                    href={project.gitUrl}
-                    className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-                  >
-                    <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-                  </Link>
-                  <Link
-                    href={project.previewUrl}
-                    className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-                  >
-                    <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-                  </Link>
-                </div> */}
-            </div>
-            {/* <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
-              <h5 className="text-xl font-semibold mb-2">{project.title}</h5>
-              <p className="text-[#ADB7BE]">{project.description}</p>
-            </div> */}
-          </div>
-        </>
-        {/* ))} */}
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
       </ul>
     </section>
   );
