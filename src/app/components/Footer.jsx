8e6a098b75dbae9fdc4b8272628d5d9fb8b754ca
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-09-23 22:20:40 +0530

    my-portfolio.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-5 flex justify-between text-slate-600 w-full">
         <span className="text-xs w-40 sm:text-sm">Made with ❤️</span>
-        <p className="text-slate-600 text-sm">
+        <p className="text-slate-600 text-xs">
           Copyright © 2023 Chandu Reddy. All rights reserved.
         </p>
       </div>
