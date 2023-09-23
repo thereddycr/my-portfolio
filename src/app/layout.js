e59073659deb4c88793f5df4ce955291de01cb9a
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-09-23 13:04:49 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* <!-- Google Analytics --> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q44W9VFD6Q"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Q44W9VFD6Q');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Fuggles, Shadows_Into_Light, Amatic_SC } from "next/font/google";
 import Image from "next/image";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -7,18 +10,71 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Achievements from "./components/Achievements";
 
+export const fuggles = Fuggles({
+  subsets: ["vietnamese"],
+  weight: "400",
+});
+
+export const shadows_into_light = Shadows_Into_Light({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const amatic_sc = Amatic_SC({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const SplashScreen = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontSize: "15px",
+        fontFamily: "monospace",
+      }}
+      // className={amatic_sc.className}
+    >
+      [ Chandu Reddy ]
+    </div>
+  );
+};
+
 export default function Main() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div class="container mt-24 mx-auto py-4 px-20">
-        <Home />
-        {/* <Achievements /> */}
-        <About />
-        <Projects />
-        <Contact />
-      </div>
-      <Footer />
+      {loading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Navbar />
+          <div class="container mt-24 mx-auto py-4 px-20">
+            <Home />
+            {/* <Achievements /> */}
+            <About />
+            <Projects />
+            <Contact />
+          </div>
+          <Footer />
+        </>
+      )}
     </main>
   );
 }
