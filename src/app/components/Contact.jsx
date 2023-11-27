b90cafc0304a71af0f4eb2f22bd117f294190eb6
thereddycr <chandrakanthreddysurikanti@gmail.com>
2023-11-27 15:46:34 +0530

    updated my resume.

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -33,11 +33,7 @@ const Contact = () => {
   };
 
   return (
-    <section
-      id="contact"
-      className=" my-12 md:my-12 py-24 gap-4"
-      // className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
+    <section id="contact" className=" my-12 md:my-12 py-24 gap-4">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
