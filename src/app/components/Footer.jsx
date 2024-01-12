f4dc5cac0ef46e19706cdb3b298bd967778c0d71
thereddycr <chandrakanthreddysurikanti@gmail.com>
2024-01-12 11:39:43 +0530

    updated about section and included new project -Nuke.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container p-5 flex justify-between text-slate-600 w-full">
         <span className="text-xs w-40 sm:text-sm">Made with ❤️</span>
         <p className="text-slate-600 text-xs">
-          Copyright © 2023 Chandu Reddy. All rights reserved.
+          Copyright © 2024 Chandu Reddy. All rights reserved.
         </p>
       </div>
     </footer>
